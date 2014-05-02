422c5500faba5046c6f3a18d0fadba3476385657
cjmf <pbj23000@gmail.com>
2014-05-01 19:01:18 -0600

    code for server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = Hapi.createServer('localhost', 8000);
+
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+
+        reply('hello world');
+    }
+});
+
+// Start the server
+server.start();
